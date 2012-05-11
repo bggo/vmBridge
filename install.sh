085811724491694f999332b7581df1bac46f580a
Bruno Gurgel <bgoliveira@sp.r7.com>
2012-05-11 14:44:04 -0300

    Add a root check and some install script! QnD

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
-#TODO
+#!/bin/bash
 
-# Ask for root
-# Copy the script to /etc/init.d/
+check_root() {
 
+if [ $(id -u) -ne 0 ]; then
+	echo "Not Root!!!!"
+	exit 1
+fi
 
+}
+
+copy_init_script() {
+	
+	cp src/vmbridge /etc/init.d/
+
+}
+
+# TODO check parmissions
+
+check_root
+
+copy_init_script
